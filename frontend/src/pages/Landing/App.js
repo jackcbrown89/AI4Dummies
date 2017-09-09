2753bd3ecb0c1dbfa2069e0ccbbaf29b33bfc8bf
Dani carmona serrat <danielcarmona55@gmail.com>
2017-09-09 03:30:41 -0400

    Add landing with csv upload

--- frontend/src/pages/Landing/App.js
+++ frontend/src/pages/Landing/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import logo from '../../assets/AI4dummies.png';
 import './App.css';
 import { Button } from 'semantic-ui-react'
+import ReactFileReader from 'react-file-reader';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class App extends Component {
+  handleFiles = files => {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+    // Use reader.result
+    console.log(reader.result)
+    }
+    reader.readAsText(files[0]);
+  }
   render() {
     return (
       <div className="App">
@@ -11,8 +21,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         <div className="Buttons-container">
-          <Button content='Upload' icon='upload' labelPosition='right' color='red' size='huge' className='button' style={{marginRight: '40px'}}/>
-          <Button content='Predict' icon='lab' labelPosition='right' color='red' size='huge' className='button'/>
+          <Grid fluid>
+            <Row>
+              <Col xs={12} md={6}>
+                <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.csv'} style={{width: '100px'}}>
+                  <Button content='Upload' icon='upload' labelPosition='right' color='red' size='huge' className='upload-button'/>
+                </ReactFileReader>
+              </Col>
+              <Col xs={12} md={6}>
+                <Button content='Predict' icon='lab' labelPosition='right' color='red' size='huge' className='predict-button'/>
+              </Col>
+            </Row>
+          </Grid>
         </div>
         {/* <div className="footer">
 
