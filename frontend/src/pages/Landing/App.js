bc5ba2ffca5a9cb7b4d34812281136d621616f2d
Dani carmona serrat <danielcarmona55@gmail.com>
2017-09-09 03:08:12 -0400

    Add good looking landing page

--- frontend/src/pages/Landing/App.js
+++ frontend/src/pages/Landing/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/AI4dummies.png';
 import './App.css';
+import { Button } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
@@ -8,11 +9,14 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Buttons-container">
+          <Button content='Upload' icon='upload' labelPosition='right' color='red' size='huge' className='button' style={{marginRight: '40px'}}/>
+          <Button content='Predict' icon='lab' labelPosition='right' color='red' size='huge' className='button'/>
+        </div>
+        {/* <div className="footer">
+
+        </div> */}
       </div>
     );
   }
