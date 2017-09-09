ba7b329464e25730b6afbf73ee1be02fc206cd38
Dani carmona serrat <danielcarmona55@gmail.com>
2017-09-09 10:27:07 -0400

    Add Save component

--- frontend/src/pages/Landing/Save/index.js
+++ frontend/src/pages/Landing/Save/index.js
@@ -2,12 +2,10 @@ import React from 'react'
 import './save.css'
 import { Button } from 'semantic-ui-react'
 
-const Save = (props) => {
-  return (
+const Save = (props) => (
     <div className='save-container'>
       <Button content='Download trained model' labelPosition='right' icon='download' size='massive' color='red'/>
     </div>
-  )
-}
+)
 
 export default Save
